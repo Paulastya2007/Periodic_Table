14c72727b10688fdc22eeb22c443991875fbec05
PAULASTYA CHAKRABORTY <86978708+Paulastya2007@users.noreply.github.com>
2022-08-14 22:05:41 +0530

    just started

---
+++ Assets/period.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class period : MonoBehaviour
+{
+    Dictionary<string, int> d = new Dictionary<string, int>();
+    void Start()
+    {        Entry(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Entry() {
+        d.Add("Hydrogen",1);
+        d.Add("Helium", 2);
+        d.Add("Lithium", 3);
+        d.Add("Beryllium", 4);
+        d.Add("Boron", 5);
+        d.Add("Carbon", 6);
+        d.Add("Nitrogen", 7);
+        d.Add("Oxygen", 8);
+        d.Add("Fluorine", 9);
+        d.Add("Neon", 10);
+        d.Add("Sodium", 11);
+        d.Add("Magnesium", 12);
+        d.Add("Aluminium", 13);
+        d.Add("Silicon", 14);
+        d.Add("Phosphorus", 15);
+        d.Add("Sulphur", 16);
+        d.Add("Chlorine", 17);
+        d.Add("Argon", 18);
+        d.Add("Potassium", 19);
+        d.Add("Calcium", 20);
+        d.Add("Scandium", 21);
+        d.Add("Titanim", 22);
+        d.Add("Vanadium", 23);
+        d.Add("Chromium", 24);
+        d.Add("Manganese", 25);
+        d.Add("Iron", 26);
+        d.Add("Cobalt", 27);
+        d.Add("Nickel", 28);
+        d.Add("Copper", 29); d.Add("Zinc", 30);
+        d.Add("Gallium", 31);
+        d.Add("Germanium", 32);
+        d.Add("Arsenic", 33);
+d.Add("Selenium", 34);
+d.Add("Bromine", 35);
+d.Add("Krypton", 36);
+d.Add("Rubidium", 37);d.Add("Strontium", 38);
+d.Add("Yttrium", 39);
+d.Add("Zirconim", 40);
+d.Add("Niobium", 41);
+d.Add("Molybdenum", 42);
+d.Add("Technetium", 43);
+d.Add("Ruthenium", 44);
+d.Add("Rhodium", 45);
+d.Add("Palladium", 46);
+d.Add("Silver", 47);
+d.Add("Cadmium", 48);
+d.Add("Indium", 49);
+d.Add("Tin", 50);
+d.Add("Antimony", 51);
+d.Add("Tellurium", 52);
+d.Add("Iodine", 53);
+d.Add("Xenon", 54);
+d.Add("Caesium", 55);
+d.Add("Barium", 56);d.Add("Hafnium", 72);d.Add("Tantalum", 73);
+d.Add("Tungstem", 74);d.Add("Rhenium", 75);
+d.Add("Osmium", 75);
+d.Add("Iridium", 77);
+d.Add("Platinum", 78);
+d.Add("Gold", 79);
+d.Add("Mercury", 80);
+d.Add("Thallium", 81);
+d.Add("Lead", 82);
+d.Add("Bismuth", 83);
+d.Add("Polonium", 84);
+d.Add("Astatine", 85);
+d.Add("Radon", 86);
+d.Add("Francium", 87);
+d.Add("Radium", 88);
+d.Add("Rutherfordium", 104);
+d.Add("Dubnium", 105);
+d.Add("Seaborguim", 106);
+d.Add("Bohrium", 107);
+d.Add("Hassium", 108);
+d.Add("Meitnerium", 109);
+d.Add("Damstacium", 110);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+d.Add("Arsenic", 33);
+    }
+}
